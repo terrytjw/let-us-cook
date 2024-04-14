7cb78817a6595191f89f50b97108512eed009992
terrytjw <tanjunweiterry@gmail.com>
2024-04-14 19:37:24 +0800

    add mobile responsiveness to footer

--- components/navigation/Footer.tsx
+++ components/navigation/Footer.tsx
@@ -11,10 +11,10 @@ const Footer = ({ className }: FooterProps) => {
     <footer className={cn(className)}>
       <div className="container flex flex-col items-center justify-between gap-4 py-10 md:h-24 md:flex-row md:py-0">
         <div className="flex flex-col items-center gap-4 px-8 md:flex-row md:gap-2 md:px-0">
-          <Link href="/" className="hidden items-center space-x-2 md:flex">
+          <Link href="/" className="flex items-center space-x-2">
             <Icons.chevronsDown />
           </Link>
-          <p className="pr-2 text-gray-500"> | </p>
+          <p className="hidden pr-2 text-gray-500 md:block"> | </p>
           <p className="text-center text-sm leading-loose text-gray-500 md:text-left">
             Built with ❤️ by{" "}
             <Link
