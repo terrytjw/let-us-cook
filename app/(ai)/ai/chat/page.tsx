027ea80461a5752be19866023424e79581171a29
terrytjw <tanjunweiterry@gmail.com>
2024-04-20 17:10:11 +0800

    add chat page description

--- app/(ai)/ai/chat/page.tsx
+++ app/(ai)/ai/chat/page.tsx
@@ -7,6 +7,13 @@ const ChatPage = () => {
     <main className="flex flex-col justify-center p-6">
       <h1 className="text-3xl font-bold">Chat with AI</h1>
       <section className="py-4">
+        <p>
+          Current supported providers include Anthropic, Groq, Ollama & OpenAI.
+          Toggle between them in{" "}
+          <span className="rounded border border-gray-400 p-1 font-mono text-gray-400">
+            /api/chat/route.ts
+          </span>
+        </p>
         <Chat />
       </section>
     </main>
