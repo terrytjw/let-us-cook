50245e1a2cb0ef90ea2a8b43b433c27cb9e0540b
terrytjw <tanjunweiterry@gmail.com>
2024-05-15 14:05:26 +0800

    fix vercel function timeout via page

--- app/(ai)/ai/code-gen/page.tsx
+++ app/(ai)/ai/code-gen/page.tsx
@@ -2,6 +2,8 @@ import { AI } from "@/lib/code-gen/actions";
 
 import CodeGenChat from "@/components/ai/code-gen/CodeGenChat";
 
+export const maxDuration = 60;
+
 const CodeGenPage = () => {
   return (
     <AI>
